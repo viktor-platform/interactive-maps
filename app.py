32b911bdaec08aaf7abb7a813d918ba4e046036c
Raoul de Jonge <rdejonge@viktor.ai>
2024-11-05 16:53:17 +0100

    update to latest sdk version

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from viktor import ViktorController
-from viktor.parametrization import ViktorParametrization, GeoPointField, Text
-from viktor.views import MapPolygon, MapResult, MapPoint, MapView, MapLine, Color
+import viktor as vkt
 
 
-class Parametrization(ViktorParametrization):
-    intro = Text("""
+class Parametrization(vkt.ViktorParametrization):
+    intro = vkt.Text("""
 # ✈️ Flight Analysis App! 
 
 In this app, you can analyze a flight path and whether it passes through a no-fly zone.
@@ -12,26 +10,26 @@ In this app, you can analyze a flight path and whether it passes through a no-fl
 **Select two points** corresponding to the airports of departure and arrival.
     """)
 
-    dep_airport = GeoPointField('Departing Airport')
-    arr_airport = GeoPointField('Arriving Airport')
+    dep_airport = vkt.GeoPointField('Departing Airport')
+    arr_airport = vkt.GeoPointField('Arriving Airport')
 
 
-class Controller(ViktorController):
+class Controller(vkt.ViktorController):
     label = 'My Map App'
     parametrization = Parametrization
 
-    @MapView('Flight Analysis', duration_guess=1)
+    @vkt.MapView('Flight Analysis', duration_guess=1)
     def generate_map(self, params, **kwargs):
         features = []
 
         # Draw no-fly zone
 
-        no_fly_zone = MapPolygon([
-            MapPoint(54.814614, -26.785331),
-            MapPoint(54.610949, -15.190123),
-            MapPoint(50.824269, -15.429211),
-            MapPoint(50.864828, -26.741683)],
-            color=Color.red(),
+        no_fly_zone = vkt.MapPolygon([
+            vkt.MapPoint(54.814614, -26.785331),
+            vkt.MapPoint(54.610949, -15.190123),
+            vkt.MapPoint(50.824269, -15.429211),
+            vkt.MapPoint(50.864828, -26.741683)],
+            color=vkt.Color.red(),
             title="No-fly zone",
             description="⛔ This is a no-fly zone. Stay out of this zone!"
         )
@@ -41,17 +39,17 @@ class Controller(ViktorController):
         # Draw airports
 
         if params.dep_airport:
-            dep_point = MapPoint.from_geo_point(params.dep_airport, icon='triangle', color=Color.blue())
+            dep_point = vkt.MapPoint.from_geo_point(params.dep_airport, icon='triangle', color=vkt.Color.blue())
             features.append(dep_point)
 
         if params.arr_airport:
-            arr_point = MapPoint.from_geo_point(params.arr_airport, icon='triangle-down', color=Color.blue())
+            arr_point = vkt.MapPoint.from_geo_point(params.arr_airport, icon='triangle-down', color=vkt.Color.blue())
             features.append(arr_point)
 
         # Draw flight path
 
         if params.dep_airport and params.arr_airport:
-            flight_path = MapLine(dep_point, arr_point)
+            flight_path = vkt.MapLine(dep_point, arr_point)
             features.append(flight_path)
 
-        return MapResult(features)
+        return vkt.MapResult(features)
